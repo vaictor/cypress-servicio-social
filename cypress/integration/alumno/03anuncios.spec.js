893833c4b8d0cd4448bce49d4f24824bbc47101e
Victor Medina <vmedina@ucol.mx>
2022-05-24 18:52:05 -0500

    Iniciamos con apartado anuncios

---
+++ cypress/integration/alumno/03anuncios.spec.js
@@ -0,0 +1,25 @@
+describe('Pruebas del alumno de la plataforma educ', () => {
+    before(() => {
+      
+    })
+
+    beforeEach(() => {
+        console.log("Iniciar sesión")
+        cy.iniciarSesionDev();
+    })
+
+
+// Caso de prueba para infogral del alumno
+    it('Alumno: Anuncios en educ / listado', () => {
+        cy.iniciarSesionDev()   
+
+        cy.get(':nth-child(6) > :nth-child(3) > .col-md-6 > .card > .course-image')
+        .click()
+
+        cy.visit('http://deveduc.ddns.net:88/alumno/anuncios/')
+        
+    })
+
+    
+
+  })
