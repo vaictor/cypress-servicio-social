6991dbf0077538e81271c60bc501414992d170a6
Victor Medina <vmedina@ucol.mx>
2022-06-02 17:14:38 -0500

    Nuevo caso de prueba para material

---
+++ cypress/integration/profesor/04material.spec.js
@@ -0,0 +1,12 @@
+// Caso de prueba para infogral educ
+describe("Material de estudio en educ", () => {
+    before(() => {});
+  
+    beforeEach(() => {
+      console.log("Iniciar sesión");
+      cy.iniciarSesionDev();
+    });
+   
+ 
+  
+  });
