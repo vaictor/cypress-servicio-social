1c2865cfb7d8e5deb9fc27ad23571029267d38e2
Francisco Fidel Castro Barreto <61883040+FedDead@users.noreply.github.com>
2022-06-08 18:07:16 -0500

    Pruebas de diferentes cursos

--- cypress/integration/alumno/05profesores.spec.js
+++ cypress/integration/alumno/05profesores.spec.js
@@ -9,37 +9,40 @@ describe('Pruebas del alumno de educ', () => {
         cy.iniciarSesionDev()
     })
 
-    // Caso de prueba para perfil de profesores
-    it('Alumno: Profesores en educ', () => {
+
+    const arrayCursos = Cypress.env('arrayCursos');
+    arrayCursos.forEach(elem => {
+        // Caso de prueba para perfil de profesores
+        it('Alumno: Profesores en educ', () => {
         
-        const confiMensaje = "Se envió este mensaje a tu profesor por correo electrónico. Espera respuesta del mismo en tu bandeja de entrada del correo Universitario"
+            const confiMensaje = "Se envió este mensaje a tu profesor por correo electrónico. Espera respuesta del mismo en tu bandeja de entrada del correo Universitario"
 
-        cy.get('#10350')
-        .click()
-        
-        cy.visit('http://deveduc.ddns.net:88/alumno/profesor/index.php')
+            cy.get('#'+elem)
+            .click()
 
-        cy.get('#btnProfesorEnviarCorreo8')
-        .click()
+            cy.visit('http://deveduc.ddns.net:88/alumno/profesor/index.php')
 
-        cy.get('#txtProfesorAsunto')
-        .type("Duda para Manzano desde cypress")
+            cy.get('.btn-info').first()
+            .click()
 
-        cy.get('#txtProfesorMensaje')
-        .type("Profesor Javier Manzano Aguilar, esto es una prueba automática desde cypess")
+            cy.get('#txtProfesorAsunto')
+            .type("Monitoreo del del aseguramiento de la calidad de Educ")
 
-        cy.get('#btnProfesorEnviar')
-        .should('be.visible')
-        .click()
+            cy.get('#txtProfesorMensaje')
+            .type("Esta es una prueba de funcionalidad realizada por el equipo de educ, si recibio este correo haga caso omiso")
 
-        cy.get('.swal2-popup')
-        cy.contains(confiMensaje).should("exist")
+            cy.get('#btnProfesorEnviar')
+            .should('be.visible')
+            .click()
 
-        cy.wait(500)
-        cy.get('.swal2-confirm')
-        .click()
+            cy.get('.swal2-popup')
+            cy.contains(confiMensaje).should("exist")
 
-        console.log('Termina el contacto con el profesor')
+            cy.wait(500)
+            cy.get('.swal2-confirm')
+            .click()
+
+            console.log('Termina el contacto con el profesor')
+        })
     })
-
-  })
+})
