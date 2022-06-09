4c083c0deb48890c47453932eaf0d5e8bb9a792d
cleon5 <cleon5@ucol.mx>
2022-06-09 17:47:27 -0500

    Anuncios con array

--- cypress/integration/alumno/03anuncios.spec.js
+++ cypress/integration/alumno/03anuncios.spec.js
@@ -1,44 +1,45 @@
-describe('Pruebas del alumno de la plataforma educ', () => {
-    before(() => {
-      
-    })
+describe("Pruebas del alumno de la plataforma educ", () => {
+  before(() => {});
 
-    beforeEach(() => {
-        console.log("Iniciar sesión")
-        cy.iniciarSesionDev();
-    })
+  beforeEach(() => {
+    console.log("Iniciar sesión");
+    cy.iniciarSesionDev();
+  });
 
-     // Caso de prueba para Anuncios del Alumno
-    it('Profesor: Anuncios en educ / Encontrar un elemento', () => {
-        cy.iniciarSesionDev()   
+  const arrayCursos = Cypress.env("arrayCursos");
+  arrayCursos.forEach((elem) => {
+    // Caso de prueba para Anuncios del Alumno
+    it("Profesor: Anuncios en educ / Encontrar un elemento", () => {
+      cy.iniciarSesionDev();
 
-        cy.get('#9285')
+      cy.get(`#${elem}`)
         .click()
 
-        cy.visit('http://deveduc.ddns.net:88/alumno/anuncios/index.php')
+      cy.visit("http://deveduc.ddns.net:88/alumno/anuncios/index.php");
 
-        cy.get(".anuncio")
-        .contains('Encuesta sobre prácticas de la materia Desarrollo de Software Seguro')
-        .should('exist')
-        
-        console.log('Termina de comprobar que haya un elemento')
-    })
+      cy.get(".anuncio")
+        .contains(
+          "Encuesta sobre prácticas de la materia Desarrollo de Software Seguro"
+        )
+        .should("exist");
+
+      console.log("Termina de comprobar que haya un elemento");
+    });
 
     // Caso de prueba para Anuncios del Alumno
-    it('Profesor: Anuncios en educ / Encontrar que no hay elementos', () =>{
-        cy.iniciarSesionDev()   
+    it("Profesor: Anuncios en educ / Encontrar que no hay elementos", () => {
+      cy.iniciarSesionDev();
 
-        cy.get('#9285')
+      cy.get(`#${elem}`)
         .click()
 
-        cy.visit('http://deveduc.ddns.net:88/alumno/anuncios/index.php')
+      cy.visit("http://deveduc.ddns.net:88/alumno/anuncios/index.php");
 
-        cy.get(".alert")
-        .contains('este curso no cuenta con información en este apartado')
-        .should('exist')
+      cy.get(".alert")
+        .contains("este curso no cuenta con información en este apartado")
+        .should("exist");
 
-        console.log('Termina de comprobar que no haya elementos')
-    })
-
-    
-  })
+      console.log("Termina de comprobar que no haya elementos");
+    });
+  });
+});
