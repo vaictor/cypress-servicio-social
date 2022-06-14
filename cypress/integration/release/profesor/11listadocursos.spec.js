f870645d40a79f8e4e6ef9ff91d3a07d8af92334
Victor Medina <vmedina@ucol.mx>
2022-06-14 15:39:42 -0500

    Listado del cursos para el profesor

---
+++ cypress/integration/release/profesor/11listadocursos.spec.js
@@ -0,0 +1,12 @@
+describe('RELEASE: Pruebas del profesor de la plataforma educ', () => {
+
+    beforeEach(() => {
+        cy.iniciarSesionDev()
+    })
+
+    it('Profesor: Listado de cursos', () => {
+
+        
+    })
+
+})
