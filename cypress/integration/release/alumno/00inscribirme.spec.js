4cd800b1561ae3e6a8be732319f1d6adfd671085
Victor Medina <vmedina@ucol.mx>
2022-06-14 09:02:09 -0500

    solicitud de curso e inscribirme

---
+++ cypress/integration/release/alumno/00inscribirme.spec.js
@@ -0,0 +1,13 @@
+describe('RELEASE: Pruebas del alumno de la plataforma educ', () => {
+
+    beforeEach(() => {
+        cy.iniciarSesionDev()
+    })
+
+    it('Alumno: Inscribirme a un curso en educ', () => {
+
+       
+        
+    })
+
+})
