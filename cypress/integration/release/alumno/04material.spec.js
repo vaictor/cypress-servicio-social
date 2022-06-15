266488a7c0ee30f85ea1b1b9e96aedc492dc5f13
Francisco Fidel Castro Barreto <61883040+FedDead@users.noreply.github.com>
2022-06-15 17:14:12 -0500

    Casos de prueba en realese material de estydui

--- cypress/integration/release/alumno/04material.spec.js
+++ cypress/integration/release/alumno/04material.spec.js
@@ -4,11 +4,21 @@ describe('RELEASE: Pruebas del alumno de la plataforma educ', () => {
         cy.iniciarSesionDev()
     })
 
-    it('Alumno: Material de estudio en educ / Comprobar que haya texto', () => {
+    const arrayCursos = Cypress.env('arrayCursos');
+    arrayCursos.forEach(elem => {
+        it('Alumno: Material de estudio en educ / Comprobar que haya texto', () => {
 
-        cy.get('#10419')
-        .click()
+            cy.get('#'+elem)
+            .click()
         
-    })
+            cy.get('li').contains('Material de estudio')
+            .click()
 
+            cy.get('.archivo').invoke('text').should('have.length.gt', 0)  // gt == greater than
+            
+            cy.wait(1000)
+            
+            console.log('Termina de comprobar que haya texto')
+        })
+    })
 })
--- cypress/integration/release/profesor/04material.spec.js
+++ cypress/integration/release/profesor/04material.spec.js
@@ -4,11 +4,334 @@ describe('RELEASE: Pruebas del alumno de la plataforma educ', () => {
         cy.iniciarSesionDev()
     })
 
-    it('Profesor: Material de estudio en educ / Comprobar que haya texto', () => {
+    const arrayCursos = Cypress.env('arrayCursos');
+    arrayCursos.forEach(elem => {
 
-        cy.get('#10419')
-        .click()
-        
+        it('Profesor: Material de estudio en educ / Crear una carpeta', () => {
+
+            cy.get('#'+elem)
+            .click()
+
+            cy.get('li').contains('Material de estudio')
+            .click()
+
+            cy.wait(1000)
+
+            cy.get('#btnGroupDrop1')
+            .click()
+
+            cy.wait(1000)
+
+            cy.get('#folder-btn')
+            .click()
+
+            cy.get('#txt_nombre')
+            .type('Carpeta cypress adjunto')
+
+            //Modal de crear carpeta, descripcion no contiene ID
+            cy.get('#folder-new > :nth-child(2) > .col-sm-6 > .form-control')
+            .type('Descripcion cypress adjunto')
+
+            //Modal de crear carpeta, select de siempre disponible no contiene ID
+            cy.get('#folder-new > :nth-child(3) > .col-sm-6 > .form-control')
+            .select('Si').should('have.value', '1')
+
+            //Modal de crear carpeta, select de carpeta de destino no contiene ID
+            cy.get('#folder-new > :nth-child(6) > .col-sm-6 > .form-control')
+            .select('Sin carpeta').should('have.value', 'raiz')
+
+            cy.get('#modal-submit')
+            .click()
+            
+            console.log('Termina de crear la carpeta')
+        })
+
+        it('Profesor: Material de estudio en educ / Crear adjunto Fuera de la carpeta', () => {
+
+            cy.get('#'+elem)
+            .click()
+
+            cy.get('li').contains('Material de estudio')
+            .click()
+
+            cy.wait(1000)
+
+            cy.get('#btnGroupDrop1')
+            .click()
+
+            cy.wait(1000)
+
+            cy.get('.dropdown-menu > :nth-child(2) > a')
+            .click()
+
+            //Falta ver donde colocar el archivo
+            //cy.get('#file_upload_drive')
+            //.attachFile("app_icon.png")
+
+             //Modal de crear adjunto, input de nombre no contiene ID
+            cy.get('#adjuntar_drive > :nth-child(2) > .col-sm-6 > .form-control')
+            .type('Nombre cypress adjunto fuera de carpeta')
+
+             //Modal de crear adjunto, input de descripcion no contiene ID
+            cy.get('#adjuntar_drive > :nth-child(3) > .col-sm-6 > .form-control')
+            .type('Descripcion cypress adjunto')
+
+            //Modal de crear adjunto, select de siempre disponible no contiene ID
+            cy.get('#adjuntar_drive > :nth-child(4) > .col-sm-6 > .form-control')
+            .select('Si').should('have.value', '1')
+
+            //Modal de crear adjunto, select de carpeta de destino no contiene ID
+            cy.get('#adjuntar_drive > :nth-child(7) > .col-sm-6 > .form-control')
+            .select('Sin carpeta').should('have.value', 'raiz')
+
+            //Modal de crear adjunto, select de enviar notificaciones no contiene ID
+            cy.get(' #enviaN_drive')
+            .select('No').should('have.value', '2')
+
+            cy.get('#modal-submit')
+            .click()
+            
+            console.log('Termina de crear el adjunto fuera de la carpeta')
+        })
+
+        it('Profesor: Material de estudio en educ / Crear adjunto dentro de la carpeta', () => {
+
+            cy.get('#'+elem)
+            .click()
+
+            cy.get('li').contains('Material de estudio')
+            .click()
+
+            cy.wait(1000)
+
+            cy.get('#btnGroupDrop1')
+            .click()
+
+            cy.wait(1000)
+
+            cy.get('.dropdown-menu > :nth-child(2) > a')
+            .click()
+
+            //Falta ver donde colocar el archivo
+            //cy.get('#file_upload_drive')
+            //.attachFile("app_icon.png")
+
+             //Modal de crear adjunto, input de nombre no contiene ID
+            cy.get('#adjuntar_drive > :nth-child(2) > .col-sm-6 > .form-control')
+            .type('Nombre cypress adjunto dentro de carpeta')
+
+             //Modal de crear adjunto, input de descripcion no contiene ID
+            cy.get('#adjuntar_drive > :nth-child(3) > .col-sm-6 > .form-control')
+            .type('Descripcion cypress adjunto')
+
+            //Modal de crear adjunto, select de siempre disponible no contiene ID
+            cy.get('#adjuntar_drive > :nth-child(4) > .col-sm-6 > .form-control')
+            .select('Si').should('have.value', '1')
+
+            //Modal de crear adjunto, select de carpeta de destino no contiene ID
+            cy.get('#adjuntar_drive > :nth-child(7) > .col-sm-6 > .form-control')
+            .select('Carpeta cypress adjunto')
+
+            //Modal de crear adjunto, select de enviar notificaciones no contiene ID
+            cy.get(' #enviaN_drive')
+            .select('No').should('have.value', '2')
+
+            cy.get('#modal-submit')
+            .click()
+            
+            console.log('Termina de crear el adjunto fuera de la carpeta')
+        })
+
+        it('Profesor: Material de estudio en educ / Crear enlace fuera de la carpeta', () => {
+
+            cy.get('#'+elem)
+            .click()
+
+            cy.get('li').contains('Material de estudio')
+            .click()
+
+            cy.wait(1000)
+
+            cy.get('#btnGroupDrop1')
+            .click()
+
+            cy.wait(1000)
+
+            cy.get('.open > .dropdown-menu > :nth-child(3) > a')
+            .click()
+
+             //Modal de crear enlace, input de nombre no contiene ID
+            cy.get('#enlace > :nth-child(1) > .col-sm-6 > .form-control')
+            .type('Nombre cypress enlace')
+
+             //Modal de crear adjunto, input de descripcion no contiene ID
+            cy.get('#enlace > :nth-child(2) > .col-sm-6 > .form-control')
+            .type('Descripcion cypress enlace fuera de la carpeta')
+
+             //Modal de crear adjunto, input de enlace no contiene ID
+            cy.get('#enlace > :nth-child(3) > .col-sm-6 > .form-control')
+            .type('https://www.youtube.com/watch?v=HDFNjDKKO6A')
+
+            //Modal de crear adjunto, select de siempre disponible no contiene ID
+            cy.get('#enlace > :nth-child(4) > .col-sm-6 > .form-control')
+            .select('Si').should('have.value', '1')
+
+            //Modal de crear adjunto, select de carpeta de destino no contiene ID
+            cy.get('#enlace > :nth-child(7) > .col-sm-6 > .form-control')
+            .select('Sin carpeta').should('have.value', 'raiz')
+
+            //Modal de crear adjunto, select de enviar notificaciones no contiene ID
+            cy.get('#enlace > :nth-child(8) > .col-sm-6 > #enviaN')
+            .select('No').should('have.value', '2')
+
+            cy.get('#modal-submit')
+            .click()
+            
+            console.log('Termina de crear el enlace fuera de la carpeta')
+        })
+
+        it('Profesor: Material de estudio en educ / Crear enlace dentro de la carpeta', () => {
+
+            cy.get('#'+elem)
+            .click()
+
+            cy.get('li').contains('Material de estudio')
+            .click()
+
+            cy.wait(1000)
+
+            cy.get('#btnGroupDrop1')
+            .click()
+
+            cy.wait(1000)
+
+            cy.get('.open > .dropdown-menu > :nth-child(3) > a')
+            .click()
+
+             //Modal de crear enlace, input de nombre no contiene ID
+            cy.get('#enlace > :nth-child(1) > .col-sm-6 > .form-control')
+            .type('Nombre cypress enlace')
+
+             //Modal de crear adjunto, input de descripcion no contiene ID
+            cy.get('#enlace > :nth-child(2) > .col-sm-6 > .form-control')
+            .type('Descripcion cypress enlace fuera de la carpeta')
+
+             //Modal de crear adjunto, input de enlace no contiene ID
+            cy.get('#enlace > :nth-child(3) > .col-sm-6 > .form-control')
+            .type('https://www.youtube.com/watch?v=HDFNjDKKO6A')
+
+            //Modal de crear adjunto, select de siempre disponible no contiene ID
+            cy.get('#enlace > :nth-child(4) > .col-sm-6 > .form-control')
+            .select('Si').should('have.value', '1')
+
+            //Modal de crear adjunto, select de carpeta de destino no contiene ID
+            cy.get('#enlace > :nth-child(7) > .col-sm-6 > .form-control')
+            .select('Carpeta cypress adjunto')
+
+            //Modal de crear adjunto, select de enviar notificaciones no contiene ID
+            cy.get('#enlace > :nth-child(8) > .col-sm-6 > #enviaN')
+            .select('No').should('have.value', '2')
+
+            cy.get('#modal-submit')
+            .click()
+            
+            console.log('Termina de crear el enlace fuera de la carpeta')
+        })
+
+        it('Profesor: Material de estudio en educ / vincular carpeta de drive fuera de carpeta', () => {
+
+            cy.get('#'+elem)
+            .click()
+
+            cy.get('li').contains('Material de estudio')
+            .click()
+
+            cy.wait(1000)
+
+            cy.get('#btnGroupDrop1')
+            .click()
+
+            cy.wait(1000)
+
+            cy.get('.dropdown-menu > :nth-child(4) > a')
+            .click()
+
+             //Modal de crear enlace, input de nombre no contiene ID
+            cy.get('#drive > :nth-child(1) > .col-sm-6 > .form-control')
+            .type('Nombre cypress enlace drive')
+
+             //Modal de crear adjunto, input de descripcion no contiene ID
+            cy.get('#drive > :nth-child(2) > .col-sm-6 > .form-control')
+            .type('Descripcion cypress enlace de drive')
+
+             //Modal de crear adjunto, input de enlace no contiene ID
+            cy.get('#drive > :nth-child(3) > .col-sm-6 > .form-control')
+            .type('https://drive.google.com/drive/folders/1X4HdOPjhGCeXuC_EO155Pa1umHTylXYq?usp=sharing')
+
+            //Modal de crear adjunto, select de siempre disponible no contiene ID
+            cy.get('#drive > :nth-child(4) > .col-sm-6 > .form-control')
+            .select('Si').should('have.value', '1')
+
+            //Modal de crear adjunto, select de carpeta de destino no contiene ID
+            cy.get('#drive > :nth-child(7) > .col-sm-6 > .form-control')
+            .select('Sin carpeta').should('have.value', 'raiz')
+
+            //Modal de crear adjunto, select de enviar notificaciones no contiene ID
+            cy.get('#drive > :nth-child(8) > .col-sm-6 > #enviaN')
+            .select('No').should('have.value', '2')
+
+            cy.get('#modal-submit')
+            .click()
+            
+            console.log('Termina de crear el enlace fuera de la carpeta')
+        })
+
+        it('Profesor: Material de estudio en educ / vincular carpeta de drive dentro de la carpeta', () => {
+
+            cy.get('#'+elem)
+            .click()
+
+            cy.get('li').contains('Material de estudio')
+            .click()
+
+            cy.wait(1000)
+
+            cy.get('#btnGroupDrop1')
+            .click()
+
+            cy.wait(1000)
+
+            cy.get('.dropdown-menu > :nth-child(4) > a')
+            .click()
+
+             //Modal de crear enlace, input de nombre no contiene ID
+            cy.get('#drive > :nth-child(1) > .col-sm-6 > .form-control')
+            .type('Nombre cypress enlace drive')
+
+             //Modal de crear adjunto, input de descripcion no contiene ID
+            cy.get('#drive > :nth-child(2) > .col-sm-6 > .form-control')
+            .type('Descripcion cypress enlace de drive')
+
+             //Modal de crear adjunto, input de enlace no contiene ID
+            cy.get('#drive > :nth-child(3) > .col-sm-6 > .form-control')
+            .type('https://drive.google.com/drive/folders/1X4HdOPjhGCeXuC_EO155Pa1umHTylXYq?usp=sharing')
+
+            //Modal de crear adjunto, select de siempre disponible no contiene ID
+            cy.get('#drive > :nth-child(4) > .col-sm-6 > .form-control')
+            .select('Si').should('have.value', '1')
+
+            //Modal de crear adjunto, select de carpeta de destino no contiene ID
+            cy.get('#drive > :nth-child(7) > .col-sm-6 > .form-control')
+            .select('Carpeta cypress adjunto')
+
+            //Modal de crear adjunto, select de enviar notificaciones no contiene ID
+            cy.get('#drive > :nth-child(8) > .col-sm-6 > #enviaN')
+            .select('No').should('have.value', '2')
+
+            cy.get('#modal-submit')
+            .click()
+            
+            console.log('Termina de crear el enlace fuera de la carpeta')
+        })
     })
-
 })
