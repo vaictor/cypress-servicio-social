f5e44aa28dcfcf9d04fc0bcb34765a7cb242dc40
Carlos Aguirre <45347452+carloseag1609@users.noreply.github.com>
2022-06-21 20:12:14 -0500

    Pruebas con alumno/actividades

--- cypress/integration/release/alumno/06actividades.spec.js
+++ cypress/integration/release/alumno/06actividades.spec.js
@@ -1,14 +1,25 @@
 describe('RELEASE: Pruebas del alumno de la plataforma educ', () => {
 
     beforeEach(() => {
-        cy.iniciarSesionDev()
+      const idCurso = "#10419";
+      cy.iniciarSesionDev();
+      cy.get(idCurso).click();
+      cy.visit(Cypress.env("devUrl") + "curso/actividades/index_admon.php");
     })
 
     it('Alumno: Actividades en educ / Comprobar que haya texto', () => {
-
-        cy.get('#10419')
-        .click()
-        
+      cy.get("#lista").children().its("length").should("be.gte", 1);
+    })
+    
+    it("Alumno: Actividades en educ / Visitar una actividad", () => {
+      cy.get("#lista")
+        .get(".select-act:first-child")
+        .first()
+        .then($el => {
+          const actividadTitulo = $el.text()
+          cy.wrap($el).click()
+          cy.get("h3").should("have.text", actividadTitulo + ' ');
+        })
     })
 
 })
