880f340c137b98ca7684812b87d3157deea10981
Victor Medina <vmedina@ucol.mx>
2022-06-29 14:29:59 -0500

    Ajuste APIs de profesor

--- cypress/integration/release/profesor/APIs/02infogral.spec.js
+++ cypress/integration/release/profesor/APIs/02infogral.spec.js
@@ -1,9 +1,191 @@
 describe("RELEASE: API infogral", () => {
 
-    it("Alumno: GET archivos HTML y adjuntos", () => {
+  let id_insertado = 0;
+  let id_insertado_comentario = 0;
+  let id_curso = 10471;
+  let id_usuario = 55859;
+
+  it("Profesor: POST archivo HTML", () => {
+    cy.request({
+        method: 'POST',
+        url: Cypress.env('devUrl')+'api/cursos/profesores/infogral/info_general',
+        headers: {
+          'content-type': 'application/json',
+          'Authorization': Cypress.env('jwtToken')
+        },
+        body: {
+          "cont":"<p>Este es un contenido de control de calidad de educ</p>\n",
+          "nombre":"GGGGG",
+          "desc":"GGGGG",
+          "dest":"raiz",
+          "arc":"/content/10471/infogral/55859_",
+          "idc":id_curso,
+          "idu":id_usuario,
+          "sec":1
+        }
+      }).as('peticionApi');
+
+      cy.get('@peticionApi').then(respuesta => {
+          expect(respuesta.status).to.eq(201);
+          expect(respuesta.body.error).to.be.false; 
+          cy.log(JSON.stringify(respuesta.body.data));
+          id_insertado = respuesta.body.data;
+      }); //get
+
+  }); // it
+
+
+
+
+  it("Profesor: POST nuevo comentario", () => {
+    cy.request({
+        method: 'POST',
+        url: Cypress.env('devUrl')+'api/cursos/profesores/infogral/comentarios',
+        headers: {
+          'content-type': 'application/json',
+          'Authorization': Cypress.env('jwtToken')
+        },
+        body: {
+          "idrec":id_insertado,
+          "comen":"PRE",
+          "idau":id_usuario,
+          "sec":1,
+          "est":0,
+          "rep":0,
+          "idmod":7,
+          "idc":id_curso
+        }
+      }).as('peticionApi');
+
+      cy.get('@peticionApi').then(respuesta => {
+          expect(respuesta.status).to.eq(201);
+          expect(respuesta.body.error).to.be.false; 
+          cy.log(JSON.stringify(respuesta.body.data));
+          id_insertado_comentario = respuesta.body.data;
+      }); //get
+
+  }); // it
+
+
+
+  it("Profesor: PATCH ordenar elementos", () => {
+    cy.request({
+        method: 'PATCH',
+        url: Cypress.env('devUrl')+'api/cursos/profesores/infogral/info_general',
+        headers: {
+          'content-type': 'application/json',
+          'Authorization': Cypress.env('jwtToken')
+        },
+        body: {
+          "raiz":[{"idArchivo":14369,"idFolder":0,"idSecuencia":"0","type":"archivo"},{"idArchivo":14375,"idFolder":0,"idSecuencia":"1","type":"archivo"},{"idArchivo":14347,"idFolder":0,"idSecuencia":"2","type":"archivo"},{"idArchivo":14373,"idFolder":0,"idSecuencia":"3","type":"archivo"}],"folders":[{"idSecuencia":"4","idFolder":956,"type":"folder","children":[]},{"idSecuencia":"5","idFolder":957,"type":"folder","children":[]},{"idSecuencia":"6","idFolder":958,"type":"folder","children":[]},{"idSecuencia":"7","idFolder":959,"type":"folder","children":[]},{"idSecuencia":"8","idFolder":960,"type":"folder","children":[]},{"idSecuencia":"9","idFolder":990,"type":"folder","children":[]},{"idSecuencia":"10","idFolder":991,"type":"folder","children":[]},{"idSecuencia":"11","idFolder":993,"type":"folder","children":[]},{"idSecuencia":"12","idFolder":995,"type":"folder","children":[]},{"idSecuencia":"13","idFolder":996,"type":"folder","children":[]},{"idSecuencia":"14","idFolder":998,"type":"folder","children":[]},{"idSecuencia":"15","idFolder":999,"type":"folder","children":[]},{"idSecuencia":"16","idFolder":1001,"type":"folder","children":[]}]
+        }
+      }).as('peticionApi');
+
+      cy.get('@peticionApi').then(respuesta => {
+          expect(respuesta.status).to.eq(200);
+          expect(respuesta.body.error).to.be.false; 
+          cy.log(JSON.stringify(respuesta.body.data));
+      }); //get
+
+  }); // it
+
+
+  it("Profesor: GET comentarios", () => {
+    cy.request({
+        method: 'GET',
+        url: Cypress.env('devUrl')+'api/cursos/profesores/infogral/curso/10346/apartado/7/comentarios',
+        headers: {
+          'content-type': 'application/json',
+          'Authorization': Cypress.env('jwtToken')
+        }
+      }).as('peticionApi');
+
+      cy.get('@peticionApi').then(respuesta => {
+          expect(respuesta.status).to.eq(200);
+          expect(respuesta.body.error).to.be.false; 
+          cy.log(JSON.stringify(respuesta.body.data));
+      }); //get
+
+  }); // it
+
+  it("Profesor: PUT reportar comentario", () => {
+    cy.request({
+        method: 'PUT',
+        url: Cypress.env('devUrl')+'api/cursos/profesores/infogral/comentarios',
+        headers: {
+          'content-type': 'application/json',
+          'Authorization': Cypress.env('jwtToken')
+        },
+        body: {
+          "id_comentario":id_insertado_comentario,
+          "modulo":7,
+          "id_reportador":id_usuario,
+          "id_curso":id_curso,
+          "nombre_curso":"UDC10471 Uso de la Plataforma EDUC para la implementaci&oacute;n de materias mediadas por tecnolog&iacute;a - Bach 34",
+          "nombre_apartado":"Información general"
+        }
+      }).as('peticionApi');
+
+      cy.get('@peticionApi').then(respuesta => {
+          expect(respuesta.status).to.eq(200);
+          expect(respuesta.body.error).to.be.false; 
+          cy.log(JSON.stringify(respuesta.body.data));
+          cy.log(id_insertado_comentario);
+      }); //get
+
+  }); // it
+
+
+  it("Profesor: DELETE eliminar comentario", () => {
+    cy.request({
+        method: 'DELETE',
+        url: Cypress.env('devUrl')+'api/cursos/profesores/infogral/comentarios',
+        headers: {
+          'content-type': 'application/json',
+          'Authorization': Cypress.env('jwtToken')
+        },
+        body: {
+          "idc":id_insertado_comentario
+        }
+      }).as('peticionApi');
+
+      cy.get('@peticionApi').then(respuesta => {
+          expect(respuesta.status).to.eq(200);
+          expect(respuesta.body.error).to.be.false; 
+          cy.log(JSON.stringify(respuesta.body.data));
+      }); //get
+
+  }); // it
+
+  
+
+/*
+  it("Profesor: POST archivo Adjunto", () => {
+    cy.request({
+        method: 'POST',
+        url: Cypress.env('devUrl')+'api/cursos/profesores/infogral/info_general',
+        headers: {
+          'content-type': 'application/json',
+          'Authorization': Cypress.env('jwtToken')
+        },
+        body: {
+          "adjunto":"Se usa un formulario normal"
+        }
+      }).as('peticionApi');
+
+      cy.get('@peticionApi').then(respuesta => {
+          expect(respuesta.status).to.eq(201);
+          expect(respuesta.body.error).to.be.false; 
+          cy.log(JSON.stringify(respuesta.body.data));
+      }); //get
+
+  }); // it
+*/
+
+    it("Profesor: GET archivos HTML y adjuntos", () => {
         cy.request({
             method: 'GET',
-            url: Cypress.env('devUrl')+'api/cursos/profesores/infogral/curso/10419/info_general',
+            url: Cypress.env('devUrl')+'api/cursos/profesores/infogral/curso/'+id_curso+'/info_general',
             headers: {
               'content-type': 'application/json',
               'Authorization': Cypress.env('jwtToken')
@@ -13,44 +195,159 @@ describe("RELEASE: API infogral", () => {
           cy.get('@peticionApi').then(respuesta => {
               expect(respuesta.status).to.eq(200);
               expect(respuesta.body.error).to.be.false; 
+              cy.log(JSON.stringify(respuesta.body.data));
           }); //get
   
       }); // it
 
-    it("Alumno: GET carpetas", () => {
-      cy.request({
-          method: 'GET',
-          url: Cypress.env('devUrl')+'api/cursos/profesores/infogral/curso/10419/carpetas',
-          headers: {
-            'content-type': 'application/json',
-            'Authorization': Cypress.env('jwtToken')
-          }
-        }).as('peticionApi');
-
-        cy.get('@peticionApi').then(respuesta => {
-            expect(respuesta.status).to.eq(200);
-            expect(respuesta.body.error).to.be.false; 
-        }); //get
-
-    }); // it
 
 
-    it("Alumno: GET comentarios", () => {
-      cy.request({
-          method: 'GET',
-          url: Cypress.env('devUrl')+'api/cursos/profesores/infogral/curso/10346/apartado/7/comentarios',
-          headers: {
-            'content-type': 'application/json',
-            'Authorization': Cypress.env('jwtToken')
-          }
-        }).as('peticionApi');
+      it("Profesor: PUT modificar archivo HTML", () => {
+        cy.request({
+            method: 'PUT',
+            url: Cypress.env('devUrl')+'api/cursos/profesores/infogral/info_general',
+            headers: {
+              'content-type': 'application/json',
+              'Authorization': Cypress.env('jwtToken')
+            },
+            body: {
+              "cont":"<p>1111111</p>\n",
+              "id_archivo":id_insertado,
+              "nombre":"Información general del curso-taller1",
+              "desc":"Extracto de la guía instruccional1",
+              "arc":"/content/10471/infogral/1_14347.html"
+            }
+          }).as('peticionApi');
+    
+          cy.get('@peticionApi').then(respuesta => {
+              expect(respuesta.status).to.eq(200);
+              expect(respuesta.body.error).to.be.false; 
+              //cy.log(JSON.stringify(respuesta.body.data));
+              cy.log(id_insertado);
+          }); //get
+    
+      }); // it
 
-        cy.get('@peticionApi').then(respuesta => {
-            expect(respuesta.status).to.eq(200);
-            expect(respuesta.body.error).to.be.false; 
-        }); //get
 
-    }); // it
+      it("Profesor: DELETE archivo HTML", () => {
+        cy.request({
+            method: 'DELETE',
+            url: Cypress.env('devUrl')+'api/cursos/profesores/infogral/info_general',
+            headers: {
+              'content-type': 'application/json',
+              'Authorization': Cypress.env('jwtToken')
+            },
+            body: {
+              "ida":id_insertado,
+              "idc":id_curso
+            }
+          }).as('peticionApi');
+    
+          cy.get('@peticionApi').then(respuesta => {
+              expect(respuesta.status).to.eq(200);
+              expect(respuesta.body.error).to.be.false; 
+              //cy.log(JSON.stringify(respuesta.body.data));
+              cy.log(id_insertado);
+          }); //get
+    
+      }); // it
 
-  });
+
+
+
+
+      it("Profesor: POST carpeta", () => {
+        cy.request({
+            method: 'POST',
+            url: Cypress.env('devUrl')+'api/cursos/profesores/infogral/carpetas',
+            headers: {
+              'content-type': 'application/json',
+              'Authorization': Cypress.env('jwtToken')
+            },
+            body: {
+              "idu":id_usuario,
+              "nombre":"PRUEBA",
+              "desc":"PRUEBA",
+              "idc":id_curso,
+              "dest":"raiz",
+              "sec":0
+            }
+          }).as('peticionApi');
   
+          cy.get('@peticionApi').then(respuesta => {
+              expect(respuesta.status).to.eq(201);
+              expect(respuesta.body.error).to.be.false; 
+              cy.log(JSON.stringify(respuesta.body.data));
+              id_insertado = respuesta.body.data;
+          }); //get
+  
+      }); // it
+
+
+    it("Profesor: GET carpetas", () => {
+      cy.request({
+          method: 'GET',
+          url: Cypress.env('devUrl')+'api/cursos/profesores/infogral/curso/'+id_curso+'/carpetas',
+          headers: {
+            'content-type': 'application/json',
+            'Authorization': Cypress.env('jwtToken')
+          }
+        }).as('peticionApi');
+
+        cy.get('@peticionApi').then(respuesta => {
+            expect(respuesta.status).to.eq(200);
+            expect(respuesta.body.error).to.be.false; 
+            cy.log(JSON.stringify(respuesta.body.data));
+        }); //get
+
+    }); // it
+
+
+    it("Profesor: PUT modificar carpeta", () => {
+      cy.request({
+          method: 'PUT',
+          url: Cypress.env('devUrl')+'api/cursos/profesores/infogral/carpetas',
+          headers: {
+            'content-type': 'application/json',
+            'Authorization': Cypress.env('jwtToken')
+          },
+          body: {
+            "idf":id_insertado,
+            "nombre":"PRUEBA1",
+            "desc":"PRUEBA1"
+          }
+        }).as('peticionApi');
+  
+        cy.get('@peticionApi').then(respuesta => {
+            expect(respuesta.status).to.eq(200);
+            expect(respuesta.body.error).to.be.false; 
+            //cy.log(JSON.stringify(respuesta.body.data));
+            cy.log(id_insertado);
+        }); //get
+  
+    }); // it
+
+
+    it("Profesor: DELETE borrar carpeta", () => {
+      cy.request({
+          method: 'DELETE',
+          url: Cypress.env('devUrl')+'api/cursos/profesores/infogral/carpetas',
+          headers: {
+            'content-type': 'application/json',
+            'Authorization': Cypress.env('jwtToken')
+          },
+          body: {
+            "idf":id_insertado
+          }
+        }).as('peticionApi');
+  
+        cy.get('@peticionApi').then(respuesta => {
+            expect(respuesta.status).to.eq(200);
+            expect(respuesta.body.error).to.be.false; 
+            //cy.log(JSON.stringify(respuesta.body.data));
+            cy.log(id_insertado);
+        }); //get
+  
+    }); // it
+  
+  });
--- cypress/integration/release/profesor/APIs/03anuncios.spec.js
+++ cypress/integration/release/profesor/APIs/03anuncios.spec.js
@@ -15,16 +15,125 @@ describe("RELEASE: API anuncios", () => {
               cy.log(respuesta.body.message);
               expect(respuesta.body.error).to.be.false; 
               cy.wrap(respuesta.body.message).should('eq','OK');
+              cy.log(JSON.stringify(respuesta.body.data));
+              
+          }); //get
+  
+      }); // it
+
+      it("Profesor: POST Nuevo elemento HTML", () => {
+        cy.request({
+            method: 'POST',
+            url: Cypress.env('devUrl')+'api/cursos/profesores/anuncios/curso/10419/anuncios/',
+            headers: {
+              'content-type': 'application/json',
+              'Authorization': Cypress.env('jwtToken')
+            },
+            body: {
+              "id_curso":"10471",
+              "modulo":"Uso de la Plataforma EDUC para la implementaci&oacute;n de materias mediadas por tecnolog&iacute;a - Bach 34",
+              "id_usuario":"55859",
+              "fechainicio":"",
+              "fechafin":"",
+              "descriptor":"prueba",
+              "anuncio":"<p>prueba</p>\n",
+              "dia":"0",
+              "siempre":"1",
+              "id_curso_multiple":[],
+              "enviarEmail":"0",
+              "usuario":"MORENTIN BALLESTEROS GUSTAVO FERNANDO ",
+              "correo":"gmorentin@ucol.mx"
+            }
+          }).as('peticionApi');
+
+          cy.get('@peticionApi').then(respuesta => {
+              expect(respuesta.status).to.eq(201);
+              cy.log(respuesta.body.message);
+              expect(respuesta.body.error).to.be.false; 
+              cy.wrap(respuesta.body.message).should('eq','Registro insertado');
+              cy.log(JSON.stringify(respuesta.body.data));
               
           }); //get
   
       }); // it
 
 
+
+
+      it("Profesor: POST Múltiple elemento HTML", () => {
+        cy.request({
+            method: 'POST',
+            url: Cypress.env('devUrl')+'api/cursos/profesores/anuncios/curso/10419/anuncios/',
+            headers: {
+              'content-type': 'application/json',
+              'Authorization': Cypress.env('jwtToken')
+            },
+            body: {
+              "id_curso":"10471",
+              "modulo":"Uso de la Plataforma EDUC para la implementaci&oacute;n de materias mediadas por tecnolog&iacute;a - Bach 34",
+              "id_usuario":"55859",
+              "fechainicio":"",
+              "fechafin":"",
+              "descriptor":"prueba",
+              "anuncio":"<p>prueba</p>\n",
+              "dia":"0",
+              "siempre":"1",
+              "id_curso_multiple":["3830|Bases de Datos Distribuidas - A","9126|CULTURA"],
+              "enviarEmail":"0",
+              "usuario":"MORENTIN BALLESTEROS GUSTAVO FERNANDO ",
+              "correo":"gmorentin@ucol.mx"
+            }
+          }).as('peticionApi');
+
+          cy.get('@peticionApi').then(respuesta => {
+              expect(respuesta.status).to.eq(201);
+              cy.log(respuesta.body.message);
+              expect(respuesta.body.error).to.be.false; 
+              cy.wrap(respuesta.body.message).should('eq','Registros insertados');
+              cy.log(JSON.stringify(respuesta.body.data));
+              
+          }); //get
+  
+      }); // it
+
+
+      it("Profesor: PUT Modificar elemento HTML", () => {
+        cy.request({
+            method: 'PUT',
+            url: Cypress.env('devUrl')+'api/cursos/profesores/anuncios/curso/10419/anuncios/',
+            headers: {
+              'content-type': 'application/json',
+              'Authorization': Cypress.env('jwtToken')
+            },
+            body: {
+              "descriptor":"prieba221212",
+              "id_anuncio":32276,
+              "anuncio":"<p>rererererer21212</p>\n",
+              "siempre":"1",
+              "dia":"0",
+              "fechainicio":"29-06-2022",
+              "fechafin":"04-07-2022",
+              "enviarMail":"0",
+              "modulo":"Uso de la Plataforma EDUC para la implementaci&oacute;n de materias mediadas por tecnolog&iacute;a - Bach 34","id_curso":"10471","correo":"gmorentin@ucol.mx",
+              "usuario":"MORENTIN BALLESTEROS GUSTAVO FERNANDO"
+            }
+          }).as('peticionApi');
+
+          cy.get('@peticionApi').then(respuesta => {
+              expect(respuesta.status).to.eq(200);
+              cy.log(respuesta.body.message);
+              expect(respuesta.body.error).to.be.false; 
+              cy.wrap(respuesta.body.message).should('eq','Registro actualizado');
+              cy.log(JSON.stringify(respuesta.body.data));
+              
+          }); //get
+  
+      }); // it
+
       it("Profesor: GET Alumnos que no han ingresado ", () => {
         cy.request({
             method: 'GET',
-            url: Cypress.env('devUrl')+'api/cursos/profesores/anuncios/curso/10419/usuario/1/actividades/',
+            url: Cypress.env('devUrl')+'api/cursos/profesores/anuncios/curso/10419/alumnos',
             headers: {
               'content-type': 'application/json',
               'Authorization': Cypress.env('jwtToken')
@@ -34,6 +143,33 @@ describe("RELEASE: API anuncios", () => {
           cy.get('@peticionApi').then(respuesta => {
               expect(respuesta.status).to.eq(200);
               expect(respuesta.body.error).to.be.false; 
+              cy.wrap(respuesta.body.message).should('eq','OK');
+              //cy.log(respuesta.body.data);
+              cy.log(JSON.stringify(respuesta.body.data));
+
+          }); //get
+  
+      }); // it
+
+
+
+      it("Profesor: GET cursos que pertenecen al usuario", () => {
+        cy.request({
+            method: 'GET',
+            url: Cypress.env('devUrl')+'api/cursos/profesores/anuncios/usuario/1/cursos/10419',
+            headers: {
+              'content-type': 'application/json',
+              'Authorization': Cypress.env('jwtToken')
+            }
+          }).as('peticionApi');
+
+          cy.get('@peticionApi').then(respuesta => {
+              expect(respuesta.status).to.eq(200);
+              cy.log(respuesta.body.message);
+              expect(respuesta.body.error).to.be.false; 
+              cy.wrap(respuesta.body.message).should('eq','OK');
+              cy.log(JSON.stringify(respuesta.body.data));
+              
           }); //get
   
       }); // it
